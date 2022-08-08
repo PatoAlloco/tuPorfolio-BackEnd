6bac3f671ce7169cdad8133f75428163b70c6e71
PatoAlloco <patoalloco13@hotmail.com>
2022-08-08 16:36:22 -0300

    agrego cambios fede

--- src/main/java/com/tuPorfolio/argentinaprograma/security/WebSecurity.java
+++ src/main/java/com/tuPorfolio/argentinaprograma/security/WebSecurity.java
@@ -3,6 +3,7 @@ package com.tuPorfolio.argentinaprograma.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 
@@ -34,15 +36,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        /*
-         * 1. Se desactiva el uso de cookies
-         * 2. Se activa la configuración CORS con los valores por defecto
-         * 3. Se desactiva el filtro CSRF
-         * 4. Se indica que el login no requiere autenticación
-         * 5. Se indica que el resto de URLs esten securizadas
-         */
+        httpSecurity.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
         httpSecurity
                 .csrf().disable().authorizeRequests()
+
                 .anyRequest().authenticated().and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
@@ -54,10 +51,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         headers.cacheControl();
         headers.frameOptions();
         headers.xssProtection();
-        for (String header : cors().getHeaders().keySet()) {
-            headers.addHeaderWriter(new StaticHeadersWriter(header, cors().getHeaders().get(header)));
-        }
-
     }
 
     @Override
@@ -65,10 +58,4 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         // Se define la clase que recupera los usuarios y el algoritmo para procesar las passwords
         auth.userDetailsService(this.authenticationConfig).passwordEncoder(passwordEncoder());
     }
-
-    @Bean
-    public HeadersCorsConfig cors() {
-        return new HeadersCorsConfig();
-    }
-
 }
